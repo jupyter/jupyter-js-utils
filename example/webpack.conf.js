17962172dcbf7f13931c596e7c4bb2bc2788f078
Steven Silvester <steven.silvester@ieee.org>
2016-01-11 15:48:03 -0600

    Add webpack file

---
+++ example/webpack.conf.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  entry: './example/build/index.js',
+  output: {
+    path: 'example/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+    ]
+  }
+}
