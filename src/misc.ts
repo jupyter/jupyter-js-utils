a01d88f63515983db5c1dcd60713643432e2a568
Steven Silvester <steven.silvester@ieee.org>
2016-01-13 13:39:38 -0600

    Allow override for wsUrl

--- src/misc.ts
+++ src/misc.ts
@@ -399,10 +399,11 @@ function getBaseUrl(): string {
  * Get the base websocket URL for a Jupyter application.
  */
 export
-function getWsUrl(): string {
+function getWsUrl(baseUrl?: string): string {
   let wsUrl = getConfigOption('wsUrl');
   if (wsUrl === void 0) {
-    wsUrl = 'ws' + getBaseUrl().slice(4);
+    baseUrl = baseUrl || getBaseUrl();
+    wsUrl = 'ws' + baseUrl.slice(4);
   }
   return wsUrl;
 }
